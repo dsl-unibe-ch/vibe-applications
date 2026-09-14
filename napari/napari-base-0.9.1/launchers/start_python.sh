af22b77415c74af0c2981024aec7ffe195dbcf03
Guillaume Witz <guillaume.witz@unibe.ch>
2026-09-14 17:21:22 +0200

    fix launchers

--- napari/napari-base-0.9.1/launchers/start_python.sh
+++ napari/napari-base-0.9.1/launchers/start_python.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-eval "$(/opt/pixi shell-hook -e napari-base-default --manifest-path /opt)"
+eval "$(/opt/pixi shell-hook -e napari-base-default --manifest-path /opt --frozen)"
 vglrun -d egl python "$@"
